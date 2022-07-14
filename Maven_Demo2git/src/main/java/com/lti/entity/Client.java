0bd78dcf455aa4cda595c7f0fb4f76257a357cca
abssinghal <78210738+abssinghal@users.noreply.github.com>
2022-07-14 12:52:52 +0530

    Create Client.java

---
+++ Maven_Demo2git/src/main/java/com/lti/entity/Client.java
@@ -0,0 +1,11 @@
+package com.lti.entity;
+
+public class Client {
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+		int r=c.add(100, 300);
+		System.out.println(r);
+		System.out.println(r);
+	}
+
+}
